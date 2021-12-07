5f63e2788a45c566a3c6cf325655d460bbe9ee92
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-12-06 22:00:48 -0300

    add: flux and conditionals

--- 02-arrays-lists-flux/src/br/com/Principal.java
+++ 02-arrays-lists-flux/src/br/com/Principal.java
@@ -13,14 +13,24 @@ public class Principal {
 //        TasksArray tasks = new TasksArray(3); utilizando arrays
         TasksList tasks = new TasksList(); // utilizando lista de arrays
 
-        Task task1 = new Task("Study");
-        Task task2 = new Task("Exercise");
-        Task task3 = new Task("Sleep");
+//        Task task1 = new Task("Study");
+//        Task task2 = new Task("Exercise");
+//        Task task3 = new Task("Sleep");
+//        Task task4 = new Task("This task is too big and should not be printed!!!");
+//       addTask was implemented in TasksList!
 
-        tasks.addTask(task1);
-        tasks.addTask(task2);
-        tasks.addTask(task3);
+        tasks.addTask("Study");
+        tasks.addTask("Eat");
+
+
+//        tasks.addTask(task1);
+//        tasks.addTask(task2);
+//        tasks.addTask(task3);
+//        tasks.addTask(task4);
         tasks.showTasks();
+
+        Task getByDescription = tasks.getTask("Study");
+        getByDescription.showTask();
     }
 }
 
--- 02-arrays-lists-flux/src/br/com/tasks/TasksList.java
+++ 02-arrays-lists-flux/src/br/com/tasks/TasksList.java
@@ -7,8 +7,28 @@ public class TasksList {
     ArrayList<Task> tasks = new ArrayList<>();
 
     public void addTask(Task task) {
-        this.tasks.add(task);
+        if (task != null && task.getTaskLength() <= 20) {
+            this.tasks.add(task);
+        } else {
+            System.out.println("Invalid task!");
+        }
     }
+
+    public void addTask(String description) {
+        Task task = new Task(description);
+        addTask(task);
+    }
+
+    public Task getTask(String description) {
+        for (Task task: tasks) {
+            if (description.equals(task.description)) {
+                return task;
+            }
+        }
+        System.out.println("Task not found!");
+        return null;
+    }
+
 
     public void removeTask(int position) {
         this.tasks.remove(position);
