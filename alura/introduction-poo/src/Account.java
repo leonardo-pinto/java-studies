5acc4652055497c284d6cc9a6e490804b740526c
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-12-07 12:13:27 -0300

    instance, attributes and references

---
+++ alura/introduction-poo/src/Account.java
@@ -0,0 +1,6 @@
+public class Account {
+    double balance;
+    int accountNumber;
+    int agency;
+    String owner;
+}
---
+++ alura/introduction-poo/src/CreateAccount.java
@@ -0,0 +1,7 @@
+public class CreateAccount {
+    public static void main(String[] args) {
+         Account account1 = new Account();
+         account1.balance = 350;
+         System.out.println(account1.balance);
+    }
+}
